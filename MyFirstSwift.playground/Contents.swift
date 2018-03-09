940fba01ff6748c69573671cb17c28611333708e
Lucy20209060 <550502661@qq.com>
2018-03-10 00:09:31 +0800

    数字类型

--- MyFirstSwift.playground/Contents.swift
+++ MyFirstSwift.playground/Contents.swift
@@ -16,3 +16,27 @@ str2 = "hello world"
 
 var num2:Int
 num2 = 99
+
+let int1:Int = 17
+let int2:Int = 0b10001
+let int3:Int = 0o21
+let int4:Int = 0x11
+
+let float_a = 0.012
+let float_b = 1.2e-2
+
+let num_a = 1000000
+let num_b = 1_000_000
+let num_c = 1_0000_0000
+
+let num_d:Float = 1
+let num_e:Int = 1
+
+let a:Int = 3
+let b:Float = 0.14159265357
+let pi:Double = Double(a) + Double(b)
+
+
+
+
+		
