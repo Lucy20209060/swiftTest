66fcd3b4782cdc57b72b2555a7128e4c1fe77e19
Lucy20209060 <550502661@qq.com>
2018-03-09 23:15:43 +0800

    常量 变量

---
+++ MyFirstSwift.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, swift"
+
+let num = 22 // 常量
+
+var index = 44  // 变量
+index = 88
+
+var x = 1, y = 2, z = 3;
+
+var str2:String
+str2 = "hello world"
+
+var num2:Int
+num2 = 99
