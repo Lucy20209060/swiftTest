4879fdfe31cbd6a3080f09ab16d6836751311ffb
Lucy20209060 <550502661@qq.com>
2018-03-12 00:37:15 +0800

    获取元组中的值及定义数据类型

--- tuples.playground/Contents.swift
+++ tuples.playground/Contents.swift
@@ -5,3 +5,24 @@ import UIKit
 var str = "Hello, playground"
 
 // 元组
+let a = ("YUMI","Luc",12)
+let (aa,bb,cc) = a
+aa
+bb
+cc
+
+a.0
+a.1
+a.2
+
+// 别名
+let b = (b1:"bb1",b2:"bb2",b3:"bb3")
+b.b2
+
+// 只需要其中一个 不需要其他的 可以用_将其他的省略掉
+let (tem,_,_) = b
+tem
+
+// 描述数据类型
+let c:(Bool,String) = (true,"大超哥")
+c.0
