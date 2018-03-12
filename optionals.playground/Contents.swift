f29101b7c69f618722e26768f4e4af4b0b6b191f
Lucy20209060 <550502661@qq.com>
2018-03-12 23:22:30 +0800

    optionals

---
+++ optionals.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// optionals 可选值
+// 或者一个值 或者没有值
+// 没有值时为nil
+
+var a:Int
+a = 1
+a
+
+var optionalsValue:Int?
+optionalsValue = 12
+
+let userInput = "abc"
+var age = userInput
